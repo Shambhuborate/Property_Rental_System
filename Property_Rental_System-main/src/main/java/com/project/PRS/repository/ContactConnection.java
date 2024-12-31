156fcfb3a1688296ae7c7b84169fd9114ced904a
Shambhuborate <149366049+Shambhuborate@users.noreply.github.com>
2024-12-31 22:39:29 +0530

    Delete Property_Rental_System-main directory

--- Property_Rental_System-main/src/main/java/com/project/PRS/repository/ContactConnection.java
+++
@@ -1,9 +0,0 @@
-package com.project.PRS.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.project.PRS.model.ContactForm;
-
-public interface ContactConnection extends JpaRepository<ContactForm, Integer> {
-	public ContactForm findById(int id);
-}
--- Property_Rental_System-main/src/main/java/com/project/PRS/repository/RequestViste.java
+++
@@ -1,9 +0,0 @@
-package com.project.PRS.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.project.PRS.model.ContactInquiry;
-
-public interface RequestViste extends JpaRepository<ContactInquiry, Integer> {
-	 ContactInquiry findByContactNumber(String contactNumber);
-}
